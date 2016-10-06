e2b1d629384e769155b84f284b038376397a48a2
Bryan <bryanye97@gmail.com>
2016-10-06 17:12:35 +1100

    Added Chapter 5 and 6

--- 05 Anonymous Functions/prefix.exs
+++ 05 Anonymous Functions/prefix.exs
@@ -1,5 +1,7 @@
 prefix = fn string1 ->
   fn string2 ->
-    string1 + string2
+    "#{string1} #{string2}"
   end
 end
+
+IO.puts prefix.("Elixir").("Rocks")
---
+++ 06 Modules and Named Functions/gcd.exs
@@ -0,0 +1,4 @@
+defmodule GCD do
+  def of(x,0), do: x
+  def of(x,y), do: gcd(y, rem(x,y))
+end
